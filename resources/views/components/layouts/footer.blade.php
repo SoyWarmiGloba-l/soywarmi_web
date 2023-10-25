f389ae0f70d629b4bcd26541e37ab57f036aa610
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-25 12:30:15 -0400

    [Add] se migró la plantilla al proyecto y cambios en la estructura con blade

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer contact-us">
+    <p>Copyright © 2023 <span style="color: red;">SoyWarmi</span> All Rights Reserved.</p>
+    {{-- <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
+    --}}
+</div>
---
+++ resources/views/components/layouts/navbar.blade.php
@@ -0,0 +1,37 @@
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="{{ route('home') }}" class="logo">
+                        <img class="w-50" src="{{ asset('assets/images/logo.png') }}" />
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="{{ route('home') }}" class="active">Inicio</a></li>
+                        <li><a href="meetings.html">Programas y Proyectos</a></li>
+                        <li class="scroll-to-section"><a href="#apply">Recursos</a></li>
+                        {{-- <li class="has-sub">
+                            <a href="javascript:void(0)">Pages</a>
+                            <ul class="sub-menu">
+                                <li><a href="meetings.html">Upcoming Meetings</a></li>
+                                <li><a href="meeting-details.html">Meeting Details</a></li>
+                            </ul>
+                        </li> --}}
+                        <li class="scroll-to-section"><a href="#courses">Nosotros</a></li>
+                        <li class="scroll-to-section"><a href="#contact">Cont&aacute;ctanos</a></li>
+                        <li class="scroll-to-section"><a href="#apply">Postula Aqu&iacute;</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
