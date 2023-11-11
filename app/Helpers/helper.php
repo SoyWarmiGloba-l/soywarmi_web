13ee3cfd1966902aeb724563f06ac4cae53a726b
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-11-11 14:51:43 -0400

    [Add] testimony views and controllers

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,17 +1,28 @@
 <?php
+
 use Illuminate\Support\Facades\Http;
 
 function authenticate()
 {
-    $response = Http::post(env('API_URL').'/api/auth/login', ['email' => env('API_EMAIL'), 'password' => env('API_PASSWORD')]);
-    return json_decode($response->body());
+    $response = Http::post(env('API_URL_GOOGLE_FIREBASE') . env('API_GOOGLE_FIREBASE'), [
+        "email" => env('API_EMAIL'),
+        "password" =>  env('API_PASSWORD'),
+        "returnSecureToken" =>  true
+    ]);
+
+    $response_auth = Http::withHeaders([
+        'Authorization' => 'Bearer ' . $response->json()["idToken"],
+    ])->post(env('API_URL_API') . '/api/auth/login', []);
+
+    return $response->json()["idToken"];
 }
 function getApiData($url, $data)
 {
-    if (empty($data)){
-        $response = Http::withToken(authenticate()->data->access_token)->get(env('API_URL'). '/api/v1/'.$url);
+    if (empty($data)) {
+        $response = Http::withToken(authenticate())->get(env('API_URL_API') . '/api/v1/' . $url);
     } else {
-        $response = Http::withToken(authenticate()->data->access_token)->post($url, $data);
+        $response = Http::withToken(authenticate())->post($url, $data);
     }
+
     return json_decode($response->body())->data;
 }
--- app/Http/Controllers/TestimonyController.php
+++ app/Http/Controllers/TestimonyController.php
@@ -9,6 +9,13 @@ class TestimonyController extends Controller
     protected $endpoint = 'testimonies';
     public function index()
     {
-//        $testimonies = $this->get();
+        $testimonies = getApiData($this->endpoint, []);
+
+        return view('all_testimonies', compact('testimonies'));
+    }
+
+    public function show($slug)
+    {
+        return view('testimony', ['testimony' => getApiData($this->endpoint . '/slug/' . $slug, [])]);
     }
 }
